53cd73f21da986aa42bb809db594212291c840c6
Durga <bhaskarbr407@gmail.com>
2024-03-16 02:51:34 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "aws_ssm_parameter" "parameter" {
   name  = "test.test1"
-  type  = "String"
   value = "hello world"
+  type  = "SecureString"
+  key_id = "48dbc6f3-656c-49e7-85ca-29fe66d3ed4d"
 }
