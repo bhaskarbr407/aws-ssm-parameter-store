a840fb9f677aa159a0ab37f5b5ec2b4a6c87a2e8
Durga <bhaskarbr407@gmail.com>
2024-03-16 17:27:16 +0530

    ansible

--- main.tf
+++ main.tf
@@ -6,3 +6,12 @@ resource "aws_ssm_parameter" "parameters" {
   key_id = "48dbc6f3-656c-49e7-85ca-29fe66d3ed4d"
   overwrite = true
 }
+
+resource "aws_ssm_parameter" "passwords" {
+  count  = length(var.passwords)
+  name   = var.passwords[count.index].name
+  value  = var.passwords[count.index].value
+  type   = "SecureString"
+  key_id = "dce90622-5a23-4f82-a639-be841f534702"
+  overwrite = true
+}
